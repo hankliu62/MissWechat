3746f9cae9fa14e3c006f84d21c928a9435b0125
‘hankliu’ <397694072@qq.com>
2016-11-21 21:43:10 +0800

    wechat: modify create jssdk signature timestamp type

--- module/index.js
+++ module/index.js
@@ -4,7 +4,7 @@
   wx.config({
     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: 'wx3ea0bef99c473ab5', // 必填，公众号的唯一标识
-    timestamp: signature.timestamp, // 必填，生成签名的时间戳
+    timestamp: parseInt(signature.timestamp, 10), // 必填，生成签名的时间戳
     nonceStr: signature.noncestr, // 必填，生成签名的随机串
     signature: signature.signature,// 必填，签名，见附录1
     jsApiList: [
