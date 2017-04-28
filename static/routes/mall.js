6452f05019df968a6c721399899aa9e747a6d1c4
hankliu <hankliu@augmentum.com.cn>
2017-04-28 12:42:21 +0800

    feat(tools): add choose tools page

--- static/routes/mall.js
+++ static/routes/mall.js
@@ -1,14 +1,25 @@
+// Hook for server
+if (typeof require.ensure !== 'function') {
+  require.ensure = function (dependencies, callback) {
+    callback(require);
+  }
+}
+
 export default {
   path: '/',
   component: function (resolve) {
-    require(['../modules/Common/Common'], resolve)
+    require.ensure([], (require) => {
+      require(['../modules/Common/Common'], resolve)
+    })
   },
   children: [
     {
       path: 'index',
       name: 'index',
       component: function (resolve) {
-        require(['../modules/Home/HomeMain'], resolve)
+        require.ensure([], function (require) {
+          require(['../modules/Home/HomeMain'], resolve)
+        })
       },
       meta: { requireWechatConfig: true, fullscreen: true }
     },
@@ -16,7 +27,9 @@ export default {
       path: 'error/:code',
       name: 'error',
       component: function (resolve) {
-        require(['../modules/Error/Error'], resolve)
+        require.ensure([], function (require) {
+          require(['../modules/Error/Error'], resolve)
+        })
       },
       meta: { fullscreen: true }
     },
--- static/routes/tools.js
+++ static/routes/tools.js
@@ -1,6 +1,13 @@
 import { PARAM_TYPES as HEX_PARAM_TYPES } from '../modules/Tools/modules/HexConverter/constants/constants'
 import { PARAM_TYPES as QRCODE_PARAM_TYPES } from '../modules/Tools/modules/QrcodeGenerator/constants/constants'
 
+// Hook for server
+if (typeof require.ensure !== 'function') {
+  require.ensure = function (dependencies, callback) {
+    callback(require);
+  }
+}
+
 const checkParamsHooks = (enableParams = []) => {
   return (to, from, next) => {
     if (to.params.type && !enableParams.includes(to.params.type)) {
@@ -13,14 +20,18 @@ const checkParamsHooks = (enableParams = []) => {
 export default {
   path: '/tools',
   component: function (resolve) {
-    require(['../modules/Tools/ToolsMain'], resolve)
+    require.ensure([], function (require) {
+      require(['../modules/Tools/ToolsMain'], resolve)
+    })
   },
   children: [
     {
       path: 'goid/:type',
       name: 'generateObjectId',
       component: function (resolve) {
-        require(['../modules/Tools/modules/GenerateObjectId/GenerateObjectIdMain'], resolve)
+        require.ensure([], function (require) {
+          require(['../modules/Tools/modules/GenerateObjectId/GenerateObjectIdMain'], resolve)
+        })
       }
     },
     { path: 'goid', redirect: 'goid/time' },
@@ -28,7 +39,9 @@ export default {
       path: 'hex/:type', // ['hex/encode', 'hex/decode']
       name: 'hexConverter',
       component: function (resolve) {
-        require(['../modules/Tools/modules/HexConverter/HexConverterMain'], resolve)
+        require.ensure([], function (require) {
+          require(['../modules/Tools/modules/HexConverter/HexConverterMain'], resolve)
+        })
       },
       beforeEnter: checkParamsHooks(Object.values(HEX_PARAM_TYPES))
     },
@@ -37,10 +50,21 @@ export default {
       path: 'qrcode/:type', // ['qrcode/text', 'qrcode/url']
       name: 'qrcodeGenerator',
       component: function (resolve) {
-        require(['../modules/Tools/modules/QrcodeGenerator/QrcodeGeneratorMain'], resolve)
+        require.ensure([], function (require) {
+          require(['../modules/Tools/modules/QrcodeGenerator/QrcodeGeneratorMain'], resolve)
+        })
       },
       beforeEnter: checkParamsHooks(Object.values(QRCODE_PARAM_TYPES))
     },
-    { path: 'qrcode', redirect: 'qrcode/text' }
+    { path: 'qrcode', redirect: 'qrcode/text' },
+    {
+      path: '/',
+      name: 'toolsIndex',
+      component: function (resolve) {
+        require.ensure([], function (require) {
+          require(['../modules/Tools/modules/Index/Index'], resolve)
+        })
+      }
+    }
   ]
 }
